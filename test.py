10857466d2b991b6bde73d6f3cc7e82c350a4fe7
= <xcrazy_starx@bk.ru>
2024-01-14 16:28:35 +0300

    comm

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-from time import sleep
-import random
-
-while True:
-    requests.get(f"https://backend.townesquare.xyz/activity/point/{random.randint(1, 100000000000000)}/A1Q09")
-    
